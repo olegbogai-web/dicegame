64ee4531c48843ad3a74d678e5bdf5e3d0bd484f
olegbogai-web <olegbogai@gmail.com>
2026-03-22 13:59:31 +0300

    Revert "Add reusable monster combat AI runtime and wire test-monster behaviour"

--- content/combat/monster_ai/monster_ai_profile.gd
+++
@@ -1,6 +0,0 @@
-extends RefCounted
-class_name MonsterAiProfile
-
-
-func decide_next_action(_context: Dictionary) -> MonsterAiAction:
-	return MonsterAiAction.create_end_turn("base_profile_default")
