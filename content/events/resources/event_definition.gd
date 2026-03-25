734591813d5e842f251169859cb4a2bf6f41671d
olegbogai-web <olegbogai@gmail.com>
2026-03-25 03:16:49 +0300

    Add scalable event room scene with test event flow

---
+++ content/events/resources/event_definition.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name EventDefinition
+
+@export var event_id: StringName
+@export var background_texture: Texture2D
+@export_multiline var event_text := ""
+@export var choices: Array[EventChoiceDefinition] = []
