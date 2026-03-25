239ae55d38e4163348625f654105f7833658ad0c
olegbogai-web <olegbogai@gmail.com>
2026-03-25 12:10:08 +0300

    Revert "Render event die outcomes with face colors instead of icon sprites"

--- content/dice/resources/dice_face_definition.gd
+++ content/dice/resources/dice_face_definition.gd
@@ -5,7 +5,6 @@ class_name DiceFaceDefinition
 enum ContentType {
 	TEXT,
 	ICON,
-	FACE_COLOR,
 }
 
 @export var content_type: ContentType = ContentType.TEXT
@@ -13,7 +12,6 @@ enum ContentType {
 @export var icon: Texture2D
 @export var text_color: Color = Color(0.0, 0.0, 0.0, 1.0)
 @export var overlay_tint: Color = Color(1.0, 1.0, 1.0, 1.0)
-@export var face_color: Color = Color(1.0, 1.0, 1.0, 1.0)
 @export_range(16, 256, 1) var font_size := 96
 @export var aura_color: Color = Color(1.0, 1.0, 1.0, 0.0)
 @export_range(0.0, 1.0, 0.01) var aura_scale := 0.32
