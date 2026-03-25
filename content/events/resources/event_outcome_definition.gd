df3fd0aa84f3c8f2041490100828487b1edfdff6
olegbogai-web <olegbogai@gmail.com>
2026-03-25 03:20:42 +0300

    Revert "Add standalone Event Room scene and test chest event flow"

--- content/events/resources/event_outcome_definition.gd
+++
@@ -1,7 +0,0 @@
-@tool
-extends Resource
-class_name EventOutcomeDefinition
-
-@export_enum("green", "yellow", "red") var color_id := "yellow"
-@export_multiline var result_text := ""
-@export var metadata: Dictionary = {}
