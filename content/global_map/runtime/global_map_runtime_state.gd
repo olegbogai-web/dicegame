1087b194b0440d465106c92867fba6d179869f16
olegbogai-web <olegbogai@gmail.com>
2026-03-26 18:46:21 +0300

    Add delayed global-map dice roll on continue transition

--- content/global_map/runtime/global_map_runtime_state.gd
+++ content/global_map/runtime/global_map_runtime_state.gd
@@ -3,6 +3,9 @@ class_name GlobalMapRuntimeState
 
 static var _has_persisted_snapshot := false
 static var _persisted_snapshot: Dictionary = {}
+static var _runtime_player: Player
+static var _has_pending_global_map_roll := false
+static var _pending_global_map_roll: Dictionary = {}
 
 var is_transition_in_progress := false
 var event_reached := false
@@ -25,3 +28,29 @@ static func load_snapshot() -> Dictionary:
 static func clear_snapshot() -> void:
 	_persisted_snapshot.clear()
 	_has_persisted_snapshot = false
+
+
+static func set_runtime_player(player: Player) -> void:
+	_runtime_player = player
+
+
+static func get_runtime_player() -> Player:
+	return _runtime_player
+
+
+static func queue_pending_global_map_roll(roll_payload: Dictionary) -> void:
+	_pending_global_map_roll = roll_payload.duplicate(true)
+	_has_pending_global_map_roll = true
+
+
+static func has_pending_global_map_roll() -> bool:
+	return _has_pending_global_map_roll
+
+
+static func consume_pending_global_map_roll() -> Dictionary:
+	if not _has_pending_global_map_roll:
+		return {}
+	var payload := _pending_global_map_roll.duplicate(true)
+	_pending_global_map_roll.clear()
+	_has_pending_global_map_roll = false
+	return payload
