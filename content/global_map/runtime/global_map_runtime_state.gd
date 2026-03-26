6cb5dad8d56d2dda68ee3ba974393d5d3a45fe45
olegbogai-web <olegbogai@gmail.com>
2026-03-26 16:01:33 +0300

    Revert "Implement Global Map screen flow: hero movement, fade and event transition"

--- content/global_map/runtime/global_map_runtime_state.gd
+++
@@ -1,8 +0,0 @@
-extends RefCounted
-class_name GlobalMapRuntimeState
-
-var hero_position: Vector3 = Vector3.ZERO
-var event_position: Vector3 = Vector3.ZERO
-var is_moving := false
-var is_transitioning := false
-var is_event_available := true
